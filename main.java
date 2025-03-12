a0e55f85aa4d92c4a197d8819efe3871a317d07b
joaopedrofreitasss <jpfdsilva@minha.fag.edu.br>
2025-03-11 21:49:23 -0300

    ola mundo criado

--- main.java
+++ main.java
@@ -1,9 +1,14 @@
 public class main {
     public static void main(String[] args) {
         helloworld();
+        olamundo();
     }
 
     public static void helloworld() {
         System.out.println("Hello World!");
+    }
+
+    public static void olamundo() {
+        System.out.println("Olá Mundo!");
     }
 }
