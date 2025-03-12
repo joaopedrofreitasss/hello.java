1bf1c4f95ef8707dba372b4dd521ed84ac67303e
joaopedrofreitasss <jpfdsilva@minha.fag.edu.br>
2025-03-11 21:53:15 -0300

    hola que tal criado

--- main.java
+++ main.java
@@ -1,9 +1,15 @@
 public class main {
     public static void main(String[] args) {
         helloworld();
+        holaquetal();
     }
 
     public static void helloworld() {
         System.out.println("Hello World!");
     }
+
+    public static void holaquetal() {
+        System.out.println("Hola que tal");
+    }
+
 }
